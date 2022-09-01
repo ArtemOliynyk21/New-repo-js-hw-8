beb52f2c3b2734864befb251d1cca36e32ae500f
ArtemOliynyk21 <temaneo700@gmail.com>
2022-09-01 08:55:17 +0300

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const markupGallery = document.querySelector('.gallery');
+
+const imageGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join(' ');
+
+markupGallery.insertAdjacentHTML('beforeend', `${imageGallery}`);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  });
 console.log(galleryItems);
