9c1671ba6ffa06a6b6318da6884944391c42d663
ArtemOliynyk21 <temaneo700@gmail.com>
2022-09-01 19:05:52 +0300

    video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const STORAGE_KEY = 'videoplayer-current-time';
+const optimizeTrottle = throttle(onPlay, 1000);
+
+const player = new Player(iframe)
+player.on('timeupdate', optimizeTrottle)
+
+function onPlay(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY)
+
+if (currentTime) {
+    player.setCurrentTime(currentTime)
+}
 
